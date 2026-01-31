ef94d21d7d2207e57b933e706b8a481bfa71c111
Maya Klein <mayaklein1999@cs.colman.ac.il>
2026-01-31 22:05:15 +0200

    create post with test

--- models/postsModel.js
+++ models/postsModel.js
@@ -3,8 +3,8 @@ const mongoose = require("mongoose");
 const postSchema = new mongoose.Schema(
   {
     title: { type: String, required: true },
-    content: { type: String, required: true },
-    owner: { type: String, required: true },
+    content: String,
+    sender: { type: String, required: true },
   },
   { timestamps: true },
 );
