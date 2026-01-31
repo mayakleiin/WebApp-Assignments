f48f2d2858ed6d34ac35e202df1d389590a19e40
Maya Klein <mayaklein1999@cs.colman.ac.il>
2026-01-31 21:11:38 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT;
+
+const mongoose = require('mongoose');
+mongoose.connect(process.env.DATABASE_URL);
+const db = mongoose.connection;
+db.on('error', (error) => console.error(error));
+db.once('open', () => console.log('Connected to Database'));
+
+app.use(express.json());
+
+const moviesRoute = require('./routes/moviesRoute');
+app.use('/movie', moviesRoute);
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
